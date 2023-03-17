23324ed0e7bd5fd4c864aeecd4c951a1bb3956f8
jinish08 <jinishshah08@gmail.com>
2023-03-17 12:39:19 +0530

    minor fix in login and pro

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -35,7 +35,7 @@ router.post("/login", async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password)
     !validPassword && res.status(400).json("wrong password")
 
-    res.status(200).json(user)
+    return res.status(200).json(user)
   } catch (err) {
     res.status(500).json(err)
   }
